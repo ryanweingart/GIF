9647652a8b1b7b19f972bb0e51da714b5c3ae487
Ryan Weingart <rmweingart5@gmail.com>
2019-07-16 16:18:54 -0400

    changed variables in js to movie stars

--- app.js
+++ app.js
@@ -1,42 +1,43 @@
 $(function(){
-    buttons(babyArray, "searchButton", "#buttons");
+    buttons(tvMovieStars, "searchButton", "#buttons");
     console.log("Page Loaded");
 })
 
-var babyArray = ["Joey Tribbiani", "Arnold Schwarzenegger", "Samuel L. Jackson"];
+var tvMovieStars = ["Joey Tribbiani", "Arnold Schwarzenegger", "Samuel L. Jackson"];
 
-function buttons(babyArray, classToAdd, buttonArea){
-    $(buttonArea).empty();
-    for(var i = 0; i < babyArray.length; i++){
-        var a = $("<button>");
-        a.addClass(classToAdd);
-        a.attr("data-type", babyArray[i]);
-        a.text(babyArray[i]);
-        $(buttonArea).append(a);
+function buttons(tvMovieStars, classToAdd, buttonArea){
+    $("#buttons").empty();
+    for(var i = 0; i < tvMovieStars.length; i++){
+        var gifButton = $("<button>");
+        gifButton.addClass(classToAdd);
+        gifButton.attr("data-stars", tvMovieStars[i]);
+        gifButton.text(tvMovieStars[i]);
+        $("#buttons").append(gifButton);
     }
 }
 
 $(document).on("click", ".searchButton", function(){
-    var type = $(this).data("type");
-    console.log(type);
-    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" +type+ "&api_key=g0S03lopEPcoMbRvxLSj9u25sydX38yF&limit=10";
-    $.ajax({url:queryURL, method: "GET"})
+    var stars = $(this).data("stars");
+    console.log(stars);
+    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" +stars+ "&api_key=g0S03lopEPcoMbRvxLSj9u25sydX38yF&limit=10";
+    $.ajax({
+        url:queryURL, 
+        method: "GET"
+    })
         .done(function(response){
             for(var i = 0; i < response.data.length; i++){
                 var searchDiv = $("<div class='search-item'>");
                 var rating = response.data[i].rating;
                 var p = $("<p>").text("Rating: " + rating);
-                var animated = response.data[i].images.fixed_height.url;
-                var still = response.data[i].images.fixed_height_still.url;
                 var image = $("<img>");
-                image.attr("src", still);
-                image.attr("data-still", still);
-                image.attr("data-animated", animated);
+                image.attr("src", response.data[i].images.fixed_height_still.url);
+                image.attr("data-still", response.data[i].images.fixed_height_still.url);
+                image.attr("data-animated", response.data[i].images.fixed_height.url);
                 image.attr("data-state", "still");
                 image.addClass("searchImage");
                 searchDiv.append(p);
                 searchDiv.append(image);
-                $("#searches").append(searchDiv);
+                $("#searches").prepend(searchDiv);
             }
         })
 })
@@ -54,7 +55,7 @@ $(document).on("click", ".searchImage", function(){
 
 $("#addSearch").on("click", function(){
     var newSearch = $("input").eq(0).val();
-    babyArray.push(newSearch);
-    buttons(babyArray, "searchButton", "#buttons");
+    tvMovieStars.push(newSearch);
+    buttons(tvMovieStars, "searchButton", "#buttons");
     return false;
 })
