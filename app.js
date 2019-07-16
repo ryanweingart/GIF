a038a3befa7dfc9755537e5fd12e720566caaf68
Ryan Weingart <rmweingart5@gmail.com>
2019-07-16 12:10:48 -0400

    function to display gifs to page

--- app.js
+++ app.js
@@ -22,6 +22,35 @@ $(document).on("click", ".searchButton", function(){
     var queryURL = "http://api.giphy.com/v1/gifs/search?q=" +type+ "&api_key=g0S03lopEPcoMbRvxLSj9u25sydX38yF&limit=10";
     $.ajax({url:queryURL, method: "GET"})
         .done(function(response){
-            
+            for(var i = 0; i < response.data.length; i++){
+                var searchDiv = $("<div class='search-item'>");
+                var rating = response.data[i].rating;
+                var p = $("<p>").text("Rating: " + rating);
+                var animated = response.data[i].images.fixed_height.url;
+                var still = response.data[i].images.fixed_height_still.url;
+                var image = $("<img>");
+                image.attr("src", still);
+                image.attr("data-still", still);
+                image.attr("data-animated", animated);
+                image.attr("data-state", "still");
+                image.addClass("searchImage");
+                searchDiv.append(p);
+                searchDiv.append(image);
+                $("#searches").append(searchDiv);
+            }
         })
+})
+
+$(document).on("click", ".searchImage", function(){
+    var state = $(this).data("state");
+    if(state === "still"){
+        $(this).attr("src", $(this).data("animated"));
+        $(this).attr("data-state", "animated");
+    }
+})
+$("#addSearch").on("click", function(){
+    var newSearch = $("input").eq(0).val();
+    babyArray.push(newSearch);
+    buttons(babyArray, "searchButton", "#buttons");
+    return false;
 })
